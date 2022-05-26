c219bf8c9ab8a0056ce54aeb76fd88cf3d32d8d5
Alessio Langellotti <alessiolangellotti2005@gmail.com>
2022-05-26 19:27:01 +0200

    morte added

--- src/children/morte.java
+++ src/children/morte.java
@@ -2,27 +2,24 @@ package children;
 
 import main.*;
 
-public class Zlange extends Batterio {
-    //posizione iniziale dei 100 batteri
+public class morte extends Batterio {
+    //inizializzazione dei 100 batteri
+    private static int nextId = 0;
     private static final int bPerRiga = (int) Math.sqrt(mainForm.getNumeroBatteriIniziali()) +
             (Math.sqrt(mainForm.getNumeroBatteriIniziali()) == 0 ? 0 : 1);
     private static final int nRighe = mainForm.getNumeroBatteriIniziali() / bPerRiga +
             (mainForm.getNumeroBatteriIniziali() % bPerRiga == 0 ? 0 : 1);
     private static final int xDistanza = Food.getWidth() / bPerRiga, yDistanza = Food.getHeight() / (nRighe);
     private static int NextX = xDistanza / 2, NextY = yDistanza / 2;
+
     //proprieta'
     private static final int visione = 200;
-    private boolean muoviSuX;
-    private boolean sensoOrario;
-    private final static int raggio = 40;
-    private final int[] centro;
+    int id;
+    private int muoviSuX; // 1 e 2 si, 0 no
+    private boolean destra, su;
+    private boolean first;
 
-
-    // mainForm.;
-    //Food.:     1024, 640
-    //Batterio.;
-
-    public Zlange() {
+    public morte() {
         x = NextX;
         y = NextY;
         NextX += xDistanza;
@@ -30,27 +27,23 @@ public class Zlange extends Batterio {
             NextY += yDistanza;
             NextX = xDistanza / 2;
         }
-        //System.out.println(x + "  " + y);
-        //System.out.println(nRighe + " " + yDistanza);
-        sensoOrario = true;
-        muoviSuX = true;
-        centro = new int[]{x, y + raggio};
+        id = nextId++;
+        muoviSuX = id % 3;
+        destra = id % 4 < 2;
+        su = id % 4 == 0 || id % 4 == 3;
     }
 
     void vaiIn(int x, int y) {
         this.x = x;
         this.y = y;
-        centro[0] = x;
-        centro[1] = y + raggio;
-        if (centro[1] > Food.getHeight())
-            centro[1] = y - raggio;
-
-        //TODO aggiorna la mappa di dov'e' il cibo
     }
 
     @Override
     protected void move() throws Exception {
-        //System.out.println("Zlange: " + getHealth());
+        if (id==0 || first) {
+            first = false;
+            return;
+        }
         int d = 1;
         int v = visione < getHealth() ? visione : getHealth() - 1;
         int guardaOgni = v / 20;
@@ -67,34 +60,20 @@ public class Zlange extends Batterio {
             d += guardaOgni/2;
         }
 
-        if (muoviSuX) {
-            if (sensoOrario)
-                x += y < centro[1] ? 1 : -1;
-            else
-                x += y < centro[1] ? -1 : +1;
-        } else {
-            if (sensoOrario)
-                y += x < centro[0] ? -1 : +1;
-            else
-                y += x < centro[0] ? +1 : -1;
-        }
-        muoviSuX = !muoviSuX;
-        if (x >= Food.getWidth() || x <= 0 || y >= Food.getHeight() || y <= 0) {
-            sensoOrario = !sensoOrario;
-            muoviSuX = !muoviSuX;
-        }
-    }
+        if (muoviSuX== 0) y += su ? -1 : 1;
+        else x += destra ? 1 : -1;
+        muoviSuX = (muoviSuX+1)%3;
 
-    private double distDaO(int x, int y) {
-        return Math.sqrt(Math.pow(x - centro[0], 2) + Math.pow(y - centro[1], 2));
+        if (x >= Food.getWidth() || x <= 0 || y >= Food.getHeight() || y <= 0){
+            destra = x < Food.getWidth()/2;
+            su = y > Food.getHeight()/2;
+        }
     }
 
     @Override
     protected Batterio clone() throws CloneNotSupportedException {
-        Zlange clone = (Zlange) super.clone();
-        clone.sensoOrario = !clone.sensoOrario;
-        if (centro[1] > y)
-            clone.centro[1] = y - raggio;
+        morte clone = (morte) super.clone();
+        clone.id = id++;
         return clone;
     }
 }
